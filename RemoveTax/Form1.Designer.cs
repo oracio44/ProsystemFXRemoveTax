dbe55199f30c629bfb78ddb5909946170b26facb
john.wine <john.wine@KS11LP519887>
2017-06-06 10:22:30 -0500

    Initial store to GitHub

---
+++ RemoveTax/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lSelected = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btRemove = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lSelected
+            // 
+            this.lSelected.AutoSize = true;
+            this.lSelected.Location = new System.Drawing.Point(93, 35);
+            this.lSelected.Name = "lSelected";
+            this.lSelected.Size = new System.Drawing.Size(29, 13);
+            this.lSelected.TabIndex = 11;
+            this.lSelected.Text = "label";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Active Folder :";
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(276, 11);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(81, 21);
+            this.btBrowse.TabIndex = 9;
+            this.btBrowse.Text = "Browse...";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // cbYear
+            // 
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Location = new System.Drawing.Point(15, 76);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(121, 21);
+            this.cbYear.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Year to Remove";
+            // 
+            // btRemove
+            // 
+            this.btRemove.Location = new System.Drawing.Point(276, 60);
+            this.btRemove.Name = "btRemove";
+            this.btRemove.Size = new System.Drawing.Size(81, 23);
+            this.btRemove.TabIndex = 14;
+            this.btRemove.Text = "Remove Year";
+            this.btRemove.UseVisualStyleBackColor = true;
+            this.btRemove.Click += new System.EventHandler(this.btRemove_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(377, 39);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Test ctx.ini written to \"ctx.iniT\".  \r\nApplication files are copied, and not dele" +
+    "ted.\r\nClient folders will need to be moved back to original location in WFX32\\\\C" +
+    "lient.\r\n";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(276, 89);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Testing";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 167);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btRemove);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbYear);
+            this.Controls.Add(this.lSelected);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Remove Tax Year";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lSelected;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btRemove;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ RemoveTax/Form1.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        string sWFX32;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string GetWFX32()
+        {
+            string strWFX32;
+            strWFX32 = Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\CCHWinFx", "NetIniLocation", "none").ToString();
+            if (strWFX32 == "none")
+                strWFX32 = Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\CCHWinFx", "Dir", "none").ToString();
+            strWFX32 = strWFX32.TrimEnd('\0');
+            if (strWFX32 == "none")
+            {
+                MessageBox.Show("Please Run Workstation Setup before running this program");
+                Application.Exit();
+            }
+            return strWFX32;
+        }
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            string sPath;
+
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                sPath = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = sPath;
+                lSelected.Text = sPath;
+                sWFX32 = sPath;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                sWFX32 = lSelected.Text = textBox1.Text;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = lSelected.Text = sWFX32 = GetWFX32();
+            fillYears();
+        }
+
+        private void fillYears()
+        {
+            string syear;
+            for (int i = 0; i < 21; i++)
+            {
+                if (i < 10)
+                    syear = 0 + i.ToString();
+                else
+                    syear = i.ToString();
+                cbYear.Items.Add(syear);
+            }
+        }
+
+        private void MoveSubdirectory(string Path, string destination)
+        {
+            string ActiveItem;
+            //For each file in directory, copy file to new destination
+            foreach (string f in Directory.GetFiles(Path))
+            {
+                ActiveItem = f.Substring(f.LastIndexOf('\\'));
+                ActiveItem = destination + ActiveItem;
+                //If file doesn't exist in destination, move the file
+                if (!File.Exists(ActiveItem))
+                {
+                    File.Move(f, ActiveItem);
+                }
+                //If file DOES exist, move file under dated file name
+                else
+                {
+                    ActiveItem = ActiveItem + DateTime.Now.Month + "-" + DateTime.Now.Day;
+                    if (File.Exists(ActiveItem))
+                    {
+                        File.Copy(f, ActiveItem, true);
+                        File.Delete(ActiveItem);
+                    }
+                    else
+                        File.Move(f, ActiveItem);
+                }
+            }
+            //Check for subdirectories.  If subdirectory does not exist in source move entire directory, else recursively call to search subdirectory
+            foreach (string g in Directory.GetDirectories(Path))
+            {
+                ActiveItem = g.Substring(g.LastIndexOf('\\'));
+                ActiveItem = destination + ActiveItem;
+                if (!Directory.Exists(ActiveItem))
+                {
+                    Directory.Move(g, ActiveItem);
+                }
+                else
+                {
+                    MoveSubdirectory(g, ActiveItem);
+                }
+            }
+            if (!checkBox1.Checked)
+                Directory.Delete(Path);
+        }
+
+        private void btRemove_Click(object sender, EventArgs e)
+        {
+            string TenYear = sWFX32 + "\\10Year";
+            string Year, TenClient, TenInstall, TenUser, Working, ActiveItem;
+            if (!Directory.Exists(TenYear))
+                Directory.CreateDirectory(TenYear);
+            if (cbYear.SelectedIndex == -1)
+                return;
+            Year = cbYear.Items[cbYear.SelectedIndex].ToString();
+            TenYear = TenYear + "\\20" + Year;
+            if (!Directory.Exists(TenYear))
+                Directory.CreateDirectory(TenYear);
+            TenClient = TenYear + "\\Client";
+            TenUser = TenYear + "\\User";
+            TenInstall = TenYear + "\\Install";
+            //Start Move of Client Return files to 10Year;
+            Working = sWFX32 + "\\Client";
+            if (!Directory.Exists(TenClient))
+                Directory.CreateDirectory(TenClient);
+            if (Directory.Exists(Working))
+            {
+
+                foreach (string d in Directory.GetDirectories(Working))
+                {
+                    ActiveItem = d.Substring(d.LastIndexOf('\\') + 1);
+                    if (ActiveItem.StartsWith(Year))
+                    {
+                        ActiveItem = TenClient + "\\" + ActiveItem;
+                        //If destination directory exists, copy all files from source folder
+                        if (Directory.Exists(ActiveItem))
+                        {
+                            string destination = ActiveItem;
+                            MoveSubdirectory(d, destination);
+                        }
+                        else
+                        {
+                            Directory.Move(d, ActiveItem);
+                        }
+                    }
+                }
+                foreach (string f in Directory.GetFiles(Working, "*.C00"))
+                {
+                    ActiveItem = f.Substring(f.LastIndexOf('\\'));
+                    ActiveItem = TenClient + ActiveItem;
+                    if (!File.Exists(ActiveItem))
+                        File.Copy(f, ActiveItem);
+                }
+            }
+            //Start Copy of Permission key from Install
+            Working = sWFX32 + "\\Install\\" + Year;
+            if (!Directory.Exists(TenInstall))
+                Directory.CreateDirectory(TenInstall);
+            if (Directory.Exists(Working))
+            {
+                TenInstall = TenInstall + "\\" + Year;
+                if (!Directory.Exists(TenInstall))
+                    Directory.CreateDirectory(TenInstall);
+                foreach (string f in Directory.GetFiles(Working))
+                {
+                    ActiveItem = f.Substring(f.LastIndexOf('\\'));
+                    ActiveItem = TenInstall + ActiveItem;
+                    if (!File.Exists(ActiveItem))
+                        File.Copy(f, ActiveItem);
+                }
+            }
+            //Start Copy of User
+            Working = sWFX32 + "\\user";
+            if (!Directory.Exists(TenUser))
+                Directory.CreateDirectory(TenUser);
+            if (Directory.Exists(Working))
+                foreach (string f in Directory.GetFiles(Working, "*.usr"))
+                {
+                    ActiveItem = f.Substring(f.LastIndexOf('\\'));
+                    ActiveItem = TenUser + ActiveItem;
+                    if (!File.Exists(ActiveItem))
+                        File.Copy(f, ActiveItem);
+                }
+            //Start Copy of ctx.ini
+            ActiveItem = sWFX32 + "\\ctx.ini";
+            if (File.Exists(ActiveItem))
+            {
+                if (!File.Exists(TenYear + "\\ctx.ini"))
+                    File.Copy(ActiveItem, TenYear + "\\ctx.ini");
+            //Remove products from CTX.ini
+            //TODO break this into smaller relevant methods for each section
+                DialogResult result = MessageBox.Show("Scan CTX.ini for entries for Year " + Year, "CTX.ini Removal", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    string input, output = "";
+                    FileStream file = new FileStream(ActiveItem, FileMode.Open, FileAccess.ReadWrite);
+                    using (StreamReader reader = new StreamReader(file, Encoding.ASCII))
+                    {
+
+                        input = reader.ReadToEnd();
+                        reader.Close();
+                    }
+                    int index, endindex;
+                    //prepare to read input line by line
+                    string line;
+                    index = 0;
+                    endindex = input.IndexOf('\r', index);
+                    while (endindex > 0 && index < input.Length)
+                    {
+
+                        line = input.Substring(index, (endindex - index));
+                        switch (line)
+                        {
+                            //remove entry from Current Release Number version list
+                            case "[Current Release Number]":
+                                {
+                                    output = output + line + "\r\n";
+                                    //Loop through lines until reaching a line that starts a new section
+                                    while (true)
+                                    {
+                                        index = endindex + 2;
+                                        endindex = input.IndexOf('\r', index);
+                                        line = input.Substring(index, (endindex - index));
+                                        if (line.StartsWith("["))
+                                            break;
+                                        //If line contains the specified year skip that line
+                                        if (!line.Contains(Year + "Version="))
+                                        {
+                                            output = output + line + "\r\n";
+                                        }
+                                    }
+                                    break;
+                                }
+                            //Remove entry for list of Installed Tax Years
+                            case "[TaxYear]":
+                                {
+                                    output = output + line + "\r\n";
+                                    //Loop through lines until reaching a line that starts a new section;
+                                    while (true)
+                                    {
+                                        index = endindex + 2;
+                                        endindex = input.IndexOf('\r', index);
+                                        line = input.Substring(index, (endindex - index));
+                                        if (line.StartsWith("["))
+                                            break;
+                                        if (line.StartsWith("TaxYears="))
+                                        {
+                                            //check for entry with comma before and after
+                                            line = line.Replace(Year + ",", "");
+                                            line = line.Replace("," + Year, "");
+                                        }
+                                        output = output + line + "\r\n";
+                                    }
+                                    break;
+                                }
+                            //Remove paths for tax products
+                            case "[paths]":
+                                {
+                                    output = output + line + "\r\n";
+                                    //Loop through lines until reaching a line that starts a new section;
+                                    while (true)
+                                    {
+                                        index = endindex + 2;
+                                        endindex = input.IndexOf('\r', index);
+                                        line = input.Substring(index, (endindex - index));
+                                        if (line.StartsWith("["))
+                                            break;
+                                        //If line starts with specified year to remove, skip
+                                        if (!line.StartsWith(Year))
+                                        {
+                                            output = output + line + "\r\n";
+                                        }
+                                    }
+                                    break;
+                                }
+                            default:
+                                {
+                                    //Line does not require special processing, Add line to ouput string, move to next line.
+                                    output = output + line + "\r\n";
+                                    index = endindex + 2;
+                                    endindex = input.IndexOf('\r', index);
+                                    break;
+                                }
+                        }
+                    }
+                    //Remove Icon for Tax Year
+                    line = "[" + Year + " Tax Preparation]";
+                    if (output.Contains(line))
+                    {
+                        index = output.IndexOf(line);
+                        endindex = output.IndexOf('[', index + 1);
+                        string beginning = output.Substring(0, index);
+                        string ending = output.Substring(endindex);
+                        output = beginning + ending;
+                    }
+                    //Check if FX Direct is installed
+                    line = Year + "WDIRECT=I";
+                    if (output.Contains(line))
+                    {
+                        index = output.IndexOf(line);
+                        endindex = output.IndexOf("\r\n", index + 1);
+                        string beginning = output.Substring(0, index);
+                        string ending = output.Substring(endindex + 1);
+                        output = beginning + ending;
+                    }
+                    //if checked, do not make permanent changes to ctx.ini, write to test file ctx.init
+                    if (checkBox1.Checked)
+                        ActiveItem = ActiveItem + "t";
+                    //write output to file
+                    using (StreamWriter writer = new StreamWriter(ActiveItem))
+                    {
+                        writer.Write(output);
+                        writer.Close();
+                    }
+                    file.Close();
+                }
+            }
+            //Remove application files
+            if (!checkBox1.Checked)
+            {
+                try
+                {
+                    ActiveItem = sWFX32 + "\\ctx" + Year + ".ini";
+                    if (File.Exists(ActiveItem))
+                        File.Delete(ActiveItem);
+                    ActiveItem = sWFX32 + "\\CtxTp" + Year + ".exe";
+                    if (File.Exists(ActiveItem))
+                        File.Delete(ActiveItem);
+                    ActiveItem = sWFX32 + "\\Tax" + Year + ".exe";
+                    if (File.Exists(ActiveItem))
+                        File.Delete(ActiveItem);
+                    ActiveItem = sWFX32 + "\\IJS.BKD";
+                    if (File.Exists(ActiveItem))
+                        File.Delete(ActiveItem);
+                    ActiveItem = sWFX32 + "\\IJS.BKX";
+                    if (File.Exists(ActiveItem))
+                        File.Delete(ActiveItem);
+                    ActiveItem = sWFX32 + "\\IJS.dat";
+                    if (File.Exists(ActiveItem))
+                        File.Delete(ActiveItem);
+                    ActiveItem = sWFX32 + "\\IJS.idx";
+                    if (File.Exists(ActiveItem))
+                        File.Delete(ActiveItem);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                ActiveItem = sWFX32 + "\\ctx" + Year + ".ini";
+                if (File.Exists(ActiveItem))
+                    //File.Delete(ActiveItem);
+                    File.Copy(ActiveItem, TenYear + "\\ctxyear.ini", true);
+                ActiveItem = sWFX32 + "\\CtxTp" + Year + ".exe";
+                if (File.Exists(ActiveItem))
+                    //File.Delete(ActiveItem);
+                    File.Copy(ActiveItem, TenYear + "\\ctxtp.exe", true);
+                ActiveItem = sWFX32 + "\\Tax" + Year + ".exe";
+                if (File.Exists(ActiveItem))
+                    //File.Delete(ActiveItem);
+                    File.Copy(ActiveItem, TenYear + "\\tax.exe", true);
+                ActiveItem = sWFX32 + "\\IJS.BKD";
+                if (File.Exists(ActiveItem))
+                    //File.Delete(ActiveItem);
+                    File.Copy(ActiveItem, TenYear + "\\ijs.bkd", true);
+                ActiveItem = sWFX32 + "\\IJS.BKX";
+                if (File.Exists(ActiveItem))
+                    //File.Delete(ActiveItem);
+                    File.Copy(ActiveItem, TenYear + "\\ijs.bkx", true);
+                ActiveItem = sWFX32 + "\\IJS.dat";
+                if (File.Exists(ActiveItem))
+                    //File.Delete(ActiveItem);
+                    File.Copy(ActiveItem, TenYear + "\\ijs.dat", true);
+                ActiveItem = sWFX32 + "\\IJS.idx";
+                if (File.Exists(ActiveItem))
+                    //File.Delete(ActiveItem);
+                    File.Copy(ActiveItem, TenYear + "\\ijs.idx", true);
+            }
+            //Prompt to run rebuild;
+            if (!checkBox1.Checked)
+            {
+                ActiveItem = sWFX32 + "\\rebuild.exe";
+                MessageBox.Show("Please run a System Rebuild \r\nUnder High-Level Options check \"Synchonize Client Links\"\r\nUnser Low-Level Options select \"Synchronize Product Types\"");
+                if (File.Exists(ActiveItem))
+                    System.Diagnostics.Process.Start(sWFX32 + "\\rebuild.exe");
+            }
+            else
+                MessageBox.Show("Test ctx.ini written to \"ctx.iniT\".\nApplication files are copied, and not deleted.\nClient folders will need to be moved back to original location in WFX32\\Client.");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Visible = checkBox1.Checked;
+        }
+    }
+}
